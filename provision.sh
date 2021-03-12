cbdbf80997f982e33ec55df5cbb987f53944a444
Gábor Erdei <65457268+gabi9669@users.noreply.github.com>
2021-03-12 13:14:23 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -4,12 +4,6 @@ sudo apt-get update
 
 sudo apt-get install -y nginx
 
-#sudo cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bkp
-
 sudo yes | cp -rf /vagrant/synced/nginx.conf /etc/nginx/nginx.conf
-
-#mkdir /usr/share/web
-
-#sudo cp /vagrant/synced/index.html /usr/share/web/
 
 sudo systemctl restart nginx.service
